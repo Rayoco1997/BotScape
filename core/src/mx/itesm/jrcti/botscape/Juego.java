38cbe8726a3e16d2941f440607fb621318fdab36
Kaliar <julio.gon.men96@gmail.com>
2017-02-12 19:41:01 -0600

    Comentario de prueba

--- core/src/mx/itesm/jrcti/botscape/Juego.java
+++ core/src/mx/itesm/jrcti/botscape/Juego.java
@@ -9,7 +9,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Juego extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	//Comentario de prueba
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
