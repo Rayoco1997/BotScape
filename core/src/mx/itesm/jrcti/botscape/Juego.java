7d0c6a77fe0e38e5e74664c3be1483b0533d0c7a
Cinthya <a01377030@itesm.mx>
2017-02-12 20:08:54 -0600

    Comenté que estuve aquí

--- core/src/mx/itesm/jrcti/botscape/Juego.java
+++ core/src/mx/itesm/jrcti/botscape/Juego.java
@@ -19,6 +19,8 @@ public class Juego extends ApplicationAdapter {
 	//comentario
 	//COMENTARIO TOMMY
 	//Comentario Julio
+	//Cinthya was here
+
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
