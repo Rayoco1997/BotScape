cce2040f840ce9eb235d934d2e252e8ed19406ab
Kaliar <julio.gon.men96@gmail.com>
2017-02-15 11:17:34 -0600

    Creación del menu de seleccion de nivel

---
+++ core/src/mx/itesm/jrcti/botscape/MenuSeleccionNivel.java
@@ -0,0 +1,48 @@
+package mx.itesm.jrcti.botscape;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Julio on 15/02/2017.
+ */
+
+public class MenuSeleccionNivel extends Pantalla {
+
+    //Imagenes a usar
+    private Texture texturaFondo;
+    private Texture texturaBtnNivel1;
+    private Texture texturaBtnNivel2;
+    private Texture texturaBtnNivel3;
+    private Texture texturaBtnRegresar;
+
+    // Ayuda a dibujar
+    private SpriteBatch batch;
+
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
