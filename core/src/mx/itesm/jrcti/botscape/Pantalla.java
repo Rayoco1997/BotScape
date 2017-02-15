91e1b1c1685c4a110fe072f5885b138b2ce07e5a
Kaliar <julio.gon.men96@gmail.com>
2017-02-15 09:40:02 -0600

    Creación de clase abstracta Pantalla para usar en menú y en niveles de juego

---
+++ core/src/mx/itesm/jrcti/botscape/Pantalla.java
@@ -0,0 +1,43 @@
+package mx.itesm.jrcti.botscape;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+/**
+ * Created by Equipo1 on 15/02/2017.
+ */
+
+public abstract class Pantalla implements Screen {
+
+    public static final float ANCHO = 1920;
+    public static final float ALTO =  1080;
+
+    protected OrthographicCamera camara;
+    protected Viewport vista;
+
+    public Pantalla(){
+        camara = new OrthographicCamera(ANCHO, ALTO);
+        camara.position.set(ANCHO/2, ALTO/2,0);
+        camara.update();
+        vista = new StretchViewport(ANCHO, ALTO, camara);
+    }
+
+    protected void borrarPantalla(){
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        vista.update(width, height);
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+}
