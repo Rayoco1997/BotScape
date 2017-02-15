9edc6321fd1b2ca006e1f3946d2bdfb319f336c1
Tommy <a01376668@itesm.mx>
2017-02-15 16:49:30 -0600

    Agregando imagenes para la pantalla pausa

--- core/src/mx/itesm/jrcti/botscape/PantallaPausa.java
+++ core/src/mx/itesm/jrcti/botscape/PantallaPausa.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
@@ -51,6 +52,22 @@ public class PantallaPausa extends Pantalla {
         TextureRegionDrawable trdButtonSeleccionarNivel= new TextureRegionDrawable(new TextureRegion(texturaButtonSelecNivel));
         TextureRegionDrawable trdButtonMenuPrincipal= new TextureRegionDrawable(new TextureRegion(texturaButtonMenuPrincipal));
 
+        //2da parte de crear botones
+        ImageButton buttonReanudar = new ImageButton(trdButtonReanudar);
+        ImageButton buttonSeleecionarNivel = new ImageButton(trdButtonSeleccionarNivel);
+        ImageButton buttonMenuPrincipal = new ImageButton(trdButtonMenuPrincipal);
+
+        //UBICANDO LOS BOTONES EN LA PANTALLA
+        buttonReanudar.setPosition(ANCHO/2-buttonReanudar.getWidth()/2,ALTO/2);
+        buttonSeleecionarNivel.setPosition(ANCHO/2-buttonSeleecionarNivel.getWidth()/2,1*ALTO/3);
+        buttonMenuPrincipal.setPosition(ANCHO/2-buttonMenuPrincipal.getWidth()/2,ALTO/6);
+
+        imgFondo.setPosition(ANCHO/2-imgFondo.getWidth()/2,5*ALTO/6);
+
+        //AGREGANDO A LA ESCENA
+        escenaPantallaPausa.addActor(buttonReanudar);
+        escenaPantallaPausa.addActor(buttonMenuPrincipal);
+        escenaPantallaPausa.addActor(buttonSeleecionarNivel);
         escenaPantallaPausa.addActor(imgFondo);
 
     }
