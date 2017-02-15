5e5e38020bb8c75d2e1da77fbba3376ce245952a
Tommy <a01376668@itesm.mx>
2017-02-15 11:38:28 -0600

    Agregando imagenes para la pantalla pausa

--- core/src/mx/itesm/jrcti/botscape/PantallaPausa.java
+++ core/src/mx/itesm/jrcti/botscape/PantallaPausa.java
@@ -1,10 +1,25 @@
 package mx.itesm.jrcti.botscape;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
 /**
  * Created by Tommy on 15/02/2017.
  */
 
 public class PantallaPausa extends Pantalla {
+    //Texturas
+    private Texture texturaFondo;
+    private Texture buttonReanudar;
+    private Texture buttonSelecNivel;
+    private Texture buttonMenuPrincipal;
+
+    //SpriteBatch
+    private SpriteBatch batch;
+
+    //escena
+    private Stage escenaPantallaPausa;
     @Override
     public void show() {
 
