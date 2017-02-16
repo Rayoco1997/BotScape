7c1f766119fc5a345c1d131d230c3ec85d789f45
Cinthya <a01377030@itesm.mx>
2017-02-15 21:18:32 -0600

    Posicionando las fotografías en el menú Credits

--- core/src/mx/itesm/jrcti/botscape/MenuCreditos.java
+++ core/src/mx/itesm/jrcti/botscape/MenuCreditos.java
@@ -64,6 +64,7 @@ public class MenuCreditos extends Pantalla {
         fotoZuren.setPosition(785,620);
         Image fotoCinth = new Image(textureCinth);
         fotoCinth.setPosition(145,620);
+
         escenaMenuCreditos.addActor(btnBack);
         escenaMenuCreditos.addActor(fotoRay);
         escenaMenuCreditos.addActor(fotoJulio);
