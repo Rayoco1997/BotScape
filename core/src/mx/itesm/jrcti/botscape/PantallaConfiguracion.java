2ae3b3d094102977fdbb190c6e4bf5b2d0836d59
Tommy <a01376668@itesm.mx>
2017-03-15 16:36:55 -0600

    Creación de la clase para la pantalla Configuracion

---
+++ core/src/mx/itesm/jrcti/botscape/PantallaConfiguracion.java
@@ -0,0 +1,32 @@
+package mx.itesm.jrcti.botscape;
+
+/**
+ * Created by Tommy on 15/03/2017.
+ */
+
+public class PantallaConfiguracion extends Pantalla {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
