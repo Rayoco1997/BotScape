47ffa553a2502a632a4ff16dcc3e17dc670c8ce8
Rayoco1997 <A01377314@itesm.mx>
2017-03-20 16:42:48 -0600

    Agregada clase Texto, adicional, agregada fuente Century Gothic

---
+++ core/src/mx/itesm/jrcti/botscape/Texto.java
@@ -0,0 +1,25 @@
+package mx.itesm.jrcti.botscape;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by rayoc on 20/03/2017.
+ */
+
+public class Texto {
+    private BitmapFont font;
+
+    public Texto(String archivo) {
+        font = new BitmapFont(Gdx.files.internal(archivo));
+    }
+
+    public void mostrarMensaje(SpriteBatch batch, String mensaje, float x, float y) {
+        GlyphLayout glyp = new GlyphLayout();
+        glyp.setText(font, mensaje);
+        float anchoTexto = glyp.width;
+        font.draw(batch, glyp, x-anchoTexto/2, y);
+    }
+}
