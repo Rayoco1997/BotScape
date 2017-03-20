0c4da5911755fa76ea1d390b2fd24b3e1645b940
Rayoco1997 <A01377314@itesm.mx>
2017-03-20 12:41:26 -0600

    Físicas añadidas a nivel tutorial

--- core/src/mx/itesm/jrcti/botscape/NivelTutorial.java
+++ core/src/mx/itesm/jrcti/botscape/NivelTutorial.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Cinthya on 15/02/2017.
